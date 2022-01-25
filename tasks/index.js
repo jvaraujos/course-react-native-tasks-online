7f3129a07bd426f65877ab40c63f11c7dbfa7e83
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-01-25 15:38:15 -0300

    165-Navegacao

--- tasks/index.js
+++ tasks/index.js
@@ -1,6 +1,6 @@
 import {AppRegistry} from 'react-native';
-import Auth from './src/screens/Auth';
+import Navigator from './src/Navigator';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler'
 
-AppRegistry.registerComponent(appName, () => Auth);
+AppRegistry.registerComponent(appName, () => Navigator);
