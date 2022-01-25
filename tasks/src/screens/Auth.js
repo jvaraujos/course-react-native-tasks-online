8a9d07bd158aac835247eece1a9fd952a9f6c43b
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-01-25 19:08:41 -0300

    170-Validando Formulario

--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -62,6 +62,18 @@ export default class App extends Component{
     }
 
     render (){
+        const validations = []
+        validations.push(this.state.email&&this.state.email.includes('@'))
+        validations.push(this.state.password&& this.state.password.length>=6)
+
+        if(this.state.stageNew){
+            validations.push(this.state.name&&this.state.name&&this.state.name.trim().length>=3)         
+            validations.push(this.state.password===this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((t,a)=> t&&a)
+
+
         return (
         <ImageBackground style={styles.background} source={backgroundImage}>
         <Text style={styles.title}>Tasks</Text>
@@ -99,8 +111,9 @@ export default class App extends Component{
             secureTextEntry
             onChangeText={confirmPassword=>this.setState({confirmPassword})}/>
             }
-            <TouchableOpacity onPress={this.signinOrSignup}>
-                <View style={styles.button}>
+            <TouchableOpacity onPress={this.signinOrSignup}
+                disabled={!validForm}>
+                <View style={[styles.button,validForm?{}:{backgroundColor:'#AAA'}]}>
                     <Text style={styles.buttonText}>
                         {this.state.stageNew?'Registrar':'Entrar'}
                         </Text>
